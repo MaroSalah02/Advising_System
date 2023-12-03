4e2bbe9cda3a1d7f21866a1b08f007b9ed0fcd34
Marawan Salah <Marawansalah002@hotmail.com>
2023-12-03 12:37:47 +0200

    Omar Riad's Code Check By Marawan

--- Omar Riad's File.sql
+++ Omar Riad's File.sql
@@ -1,4 +1,4 @@
-﻿--2.2-B
+﻿--2.2-B -- CHECKED 
 GO
 CREATE VIEW view_Course_prerequisites(Course_ID, Course_Name, Course_Major, C_is_offered, C_Credit_Hours, C_Semester, Prerequisiite_ID, Prerequisite_Name, Prerequisite_Major, P_is_offered, P_Credit_Hours, P_Semester)
 AS
@@ -7,22 +7,23 @@ LEFT OUTER JOIN PreqCourse_course pc ON c.course_id = pc.course_id
 LEFT OUTER JOIN Course p ON pc.prerequisite_course_id = p.course_id
 GO
 
---2.2-G
+--2.2-G -- CHECKED
 GO
 CREATE VIEW Students_Courses_transcript(Student_ID, First_Name, Last_Name, Course_ID, Course_Name, Exam_Type, Course_Grade, Semester, Instructor_Name)
 AS
-SELECT s.student_id, S.f_name, S.l_name, T.course_id, c.name, T.exam_type, T.grade, T.semester_code, I.name FROM Student S, Student_Instructor_Course_Take T, Course C, Instructor I
-WHERE S.student_id = T.student_id AND T.course_id = C.course_id
+SELECT s.student_id, S.f_name, S.l_name, T.course_id, c.name, T.exam_type, T.grade, T.semester_code, I.name 
+FROM Student S, Student_Instructor_Course_Take T, Course C, Instructor I
+WHERE S.student_id = T.student_id AND T.course_id = C.course_id AND I.instructor_id = T.instructor_id
 GO
 
---2.3-C
+--2.3-C -- CHECKED
 GO
 CREATE PROCEDURE Procedures_AdminListStudents
 AS
 SELECT * FROM Student
 GO
 
---2.3-H
+--2.3-H -- CHECKED
 GO
 CREATE PROCEDURE Procedures_AdminLinkInstructor
 @InstructorId int,
@@ -34,19 +35,22 @@ SET instructor_id = @InstructorId, course_id = @courseId
 WHERE slot_id = @slotID
 GO
 
---2.3-M
+--2.3-M -- CHECKED
 GO
 CREATE PROCEDURE Procedures_AdminDeleteCourse
 @courseID int
 AS
-DELETE FROM Slot
+DELETE FROM PreqCourse_course
+WHERE course_id = @courseID or prerequisite_course_id = @courseID
+
+DELETE FROM Slot 
 WHERE course_id = @courseID
 
 DELETE FROM Course
 WHERE course_id = @courseID
 GO
 
---2.3-R
+--2.3-R -- CHECKED 
 GO
 CREATE PROCEDURE Procedures_AdvisorCreateGP
 @sem_code VARCHAR(40),
@@ -59,7 +63,7 @@ INSERT INTO Graduation_Plan(semster_code, semster_credit_hours, expected_grad_da
 VALUES(@sem_code, @sem_credit_hours, @exp_grad_date, @advisor, @student)
 GO
 
---2.3-W
+--2.3-W -- CHECKED
 GO
 CREATE PROCEDURE Procedures_AdvisorApproveRejectCHRequest
 @requestID INT,
@@ -123,7 +127,7 @@ BEGIN
 END
 GO
 
---2.3-BB
+--2.3-BB -- checked 
 GO
 CREATE PROCEDURE Procedures_StudentaddMobile
 @StudentID INT,
@@ -134,21 +138,28 @@ VALUES(@StudentID,@mobile_number);
 GO
 
 
---2.3-GG
+--2.3-GG -- CHECKED
 
 go;
 create function FN_StudentUpcoming_installment
 (@StudentID int)
 returns date
 begin
-return(select i.deadline from Payment p inner join Installment i on
-p.payment_id=i.payment_id
-where p.student_id=@StudentID and i.dealine<current_timestamp and  i.deadline < all(select p.payment_id ,i.deadline from Payment p inner join Installment i on
-p.payment_id=i.payment_id))
-end;
+    declare @UpcomingDeadline date;
+
+    select top 1 @UpcomingDeadline = i.deadline
+    from Payment p
+    inner join Installment i on p.payment_id = i.payment_id
+    where p.student_id = @StudentID
+        and i.deadline > current_timestamp
+		and i.status = 'notPaid'
+	ORDER BY i.deadline ASC
+
+    return @UpcomingDeadline;
+end
 go;
 
---2.3-LL
+--2.3-LL -- Not completed
 GO
 CREATE PROCEDURE Procedures_ViewRequiredCourses
 @StudentID int,
@@ -194,11 +205,11 @@ GO
 --2.3-MM
 
 GO
-CREATE PROCEDURE Procedures_ViewOptionalCourse
+create PROCEDURE Procedures_ViewOptionalCourse
 @StudentID int,
 @Current_semester_code VARCHAR(40)
 AS
-
+	declare @odd INT
 IF @Current_semester_code LIKE '%W__%' OR @Current_semester_code LIKE '%S__R1%'
 	SET @odd = 1
 ELSE
@@ -219,12 +230,12 @@ BEGIN
 		WHERE student_id = @StudentID
 
 		SELECT C.course_id, C.name, C.major, C.is_offered, C.credit_hours, C.semester FROM Course C
-		WHERE C.major = @s_major AND C.semester >= @s_semester AND MOD(C.semester, 2) = @odd
+		WHERE C.major = @s_major AND C.semester >= @s_semester AND C.semester % 2 = @odd
 		EXCEPT
 		SELECT C.course_id, C.name, C.major, C.is_offered, C.credit_hours, C.semester FROM Course C
 		INNER JOIN PreqCourse_course PC ON C.course_id = PC.course_id
-		WHERE PC.prerequisite_course_id NOT IN (SELECT * FROM Student_Instructor_Course_Take SIC
+		WHERE PC.prerequisite_course_id NOT IN (SELECT SIC.course_id FROM Student_Instructor_Course_Take SIC
 												WHERE SIC.student_id = @StudentID)
 	END
 END
-GO
+Go
