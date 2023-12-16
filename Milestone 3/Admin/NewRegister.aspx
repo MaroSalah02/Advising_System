492f653605f8934b6e884fb3f002e370d9934c88
mohamad rageh <rageh205@gmail.com>
2023-12-16 22:32:05 +0200

    Register Page in admin folder!

---
+++ Milestone 3/Admin/NewRegister.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="NewRegister.aspx.cs" Inherits="Milestone_3.Admin.NewRegister" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+            <asp:Label ID="Label1" runat="server" Text="Do you want to register as a student?"></asp:Label>
+            <div style="display:flex; flex-direction:row; row-gap:10px;">
+                <asp:Label ID="Label2" runat="server" Text="Student Registration"></asp:Label>
+                <asp:TextBox ID="Textb1" runat="server" placeholder="enter student first_name"></asp:TextBox>
+
+                <asp:TextBox ID="Textb2" runat="server" placeholder="enter student last_name" ></asp:TextBox>
+
+                <asp:TextBox ID="Textb3" runat="server" placeholder="enter student password" ></asp:TextBox>
+
+                <asp:TextBox ID="Textb4" runat="server" placeholder="enter student faculty" ></asp:TextBox>
+
+                <asp:TextBox ID="Textb5" runat="server" placeholder="enter student email" ></asp:TextBox>
+
+                <asp:TextBox ID="Textb6" runat="server" placeholder="enter student major" ></asp:TextBox>
+
+                <asp:TextBox ID="Textb7" runat="server" placeholder="enter student Semester" type="number"></asp:TextBox>
+
+            </div>
+            <asp:Button ID="Button1" runat="server" Text="Register student" OnClick="Register_for_student_click"/>
+
+        <div>
+            <asp:TextBox ID="Textb9" runat="server" placeholder="enter advisor name"></asp:TextBox>
+
+            <asp:TextBox ID="Textb10" runat="server" placeholder="enter advisro password" ></asp:TextBox>
+
+            <asp:TextBox ID="Textb11" runat="server" placeholder="enter advisor email" ></asp:TextBox>
+
+            <asp:TextBox ID="Textb12" runat="server" placeholder="enter advisor office" ></asp:TextBox>
+        </div>
+        <asp:Button ID="Button2" runat="server" Text="Register advisor" OnClick="Register_for_advisor_click"/>
+    </form>
+</body>
+</html>
---
+++ Milestone 3/Admin/NewRegister.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Milestone_3.Admin
+{
+    public partial class NewRegister : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Register_for_student_click(object sender, EventArgs e)
+        {
+            String connectstr = WebConfigurationManager.ConnectionStrings["con"].ToString();
+            SqlConnection c = new SqlConnection(connectstr);
+            SqlCommand proc = new SqlCommand("Procedures_StudentRegistration", c);
+            proc.CommandType = CommandType.StoredProcedure;
+
+            if(Textb1.Text.Equals("") || Textb2.Text.Equals("") || Textb3.Text.Equals("") || Textb4.Text.Equals("") || Textb5.Text.Equals("") || Textb6.Text.Equals("") || Textb7.Text.Equals(""))
+            {
+                alert();
+            }
+            else
+            {
+                String first_name = Textb1.Text; 
+                proc.Parameters.Add(new SqlParameter("@first_name", first_name));
+
+                String last_name = Textb2.Text; 
+                proc.Parameters.Add(new SqlParameter("@last_name", last_name));
+
+                String password = Textb3.Text;
+                proc.Parameters.Add(new SqlParameter("@password", password));
+
+                String faculty = Textb4.Text;
+                proc.Parameters.Add(new SqlParameter("@faculty", faculty));
+                    
+                String email = Textb5.Text;
+                proc.Parameters.Add(new SqlParameter("@email", email));
+
+                String major = Textb6.Text;
+                proc.Parameters.Add(new SqlParameter("@major", major));
+
+                int Semester = Int16.Parse(Textb7.Text);
+                proc.Parameters.Add(new SqlParameter("@Semester", Semester));
+
+                SqlParameter result = new SqlParameter("@Student_id", SqlDbType.Int);
+
+                result.Direction = ParameterDirection.Output;
+
+                proc.Parameters.Add(result);
+
+                c.Open();
+                    proc.ExecuteNonQuery();
+                    Response.Write("New student id is " + result.Value.ToString());
+                c.Close();
+            }
+        }
+        private void alert()
+        {
+            string script = "alert('There is one or more empty fields!');";
+            ClientScript.RegisterStartupScript(this.GetType(), "alert", script, true);
+        }
+
+        protected void Register_for_advisor_click(object sender, EventArgs e)
+        {
+            String connectstr = WebConfigurationManager.ConnectionStrings["con"].ToString();
+            SqlConnection c = new SqlConnection(connectstr);
+            SqlCommand proc = new SqlCommand("Procedures_AdvisorRegistration", c);
+            proc.CommandType = CommandType.StoredProcedure;
+
+            if (Textb9.Text.Equals("") || Textb10.Text.Equals("") || Textb11.Text.Equals("") || Textb12.Text.Equals("")) 
+            {
+                alert();
+            }
+            else
+            {
+                String advisor_name = Textb9.Text;
+                proc.Parameters.Add(new SqlParameter("@advisor_name", advisor_name));
+
+                String password = Textb10.Text;
+                proc.Parameters.Add(new SqlParameter("@password", password));
+
+                String email = Textb11.Text;
+                proc.Parameters.Add(new SqlParameter("@email", email));
+
+                String office = Textb12.Text;
+                proc.Parameters.Add(new SqlParameter("@office", office));
+
+                SqlParameter result = new SqlParameter("@Advisor_id", SqlDbType.Int);
+
+                result.Direction = ParameterDirection.Output;
+
+                proc.Parameters.Add(result);
+
+                c.Open();
+                    proc.ExecuteNonQuery();
+                    Response.Write("New advisor id is " + result.Value.ToString());
+                c.Close();
+            }
+        }
+    }
+}
